89b1da75be60efb85beb7e0a27253a51243a63a8
Link4Electronics <link4electronics@gmail.com>
2026-04-11 20:35:25 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,10 +3,10 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q etlegacy-git | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q etlegacy-git | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/etl.svg
 export DESKTOP=/usr/share/applications/com.etlegacy.ETLegacy.$ARCH.desktop
@@ -15,13 +15,11 @@ export STARTUPWMCLASS=etl.$ARCH
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/etl.$ARCH /usr/lib/etlegacy/*
-
-# Additional changes can be done in between here
+quick-sharun /usr/bin/etl.$ARCH /usr/lib/etlegacy/* /usr/lib/libopenal.so*
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
 quick-sharun --simple-test ./dist/*.AppImage
